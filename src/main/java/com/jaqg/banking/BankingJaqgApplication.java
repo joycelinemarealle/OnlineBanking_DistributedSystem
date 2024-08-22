d2447d4b4db947f537a23939a4449396795d4a6f
George Saint Hilaire <georgesainthilaire@gmail.com>
2024-08-22 10:24:32 -0400

    Initial commit

---
+++ src/main/java/com/jaqg/banking/BankingJaqgApplication.java
@@ -0,0 +1,13 @@
+package com.jaqg.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingJaqgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingJaqgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaqg/banking/BankingJaqgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaqg.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingJaqgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
