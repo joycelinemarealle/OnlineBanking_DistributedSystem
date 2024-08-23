a2ebaf98803e34fa6a1dfc789cee2a7e1621ac2c
George Saint Hilaire <georgesainthilaire@gmail.com>
2024-08-23 14:08:33 -0400

    task FPBA-5: Created a transaction class with its setters and getters and args and no args constructor

--- src/main/java/com/jaqg/banking/entities/Transaction.java
+++ src/main/java/com/jaqg/banking/entities/Transaction.java
@@ -1,4 +1,72 @@
 package com.jaqg.banking.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
 public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY) // This helps auto-generate primry keys
+    private long id;
+    private LocalDate dateTime;
+    private BigDecimal transVal;
+    private OperationType transType; // Transaction type can be withdraw, deposit, ect...
+    private Account recipient; // to account
+    // from account (TBD because it's implied that the account handling the transaction is the account where the transaction is coming from
+
+
+    public Transaction(long id, LocalDate dateTime, BigDecimal transVal, OperationType transType, Account recipient) {
+        this.id = id;
+        this.dateTime = dateTime;
+        this.transVal = transVal;
+        this.transType = transType;
+        this.recipient = recipient;
+    }
+    public Transaction(){}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDate dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public BigDecimal getTransVal() {
+        return transVal;
+    }
+
+    public void setTransVal(BigDecimal transVal) {
+        this.transVal = transVal;
+    }
+
+    public OperationType getTransType() {
+        return transType;
+    }
+
+    public void setTransType(OperationType transType) {
+        this.transType = transType;
+    }
+
+    public Account getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(Account recipient) {
+        this.recipient = recipient;
+    }
 }
