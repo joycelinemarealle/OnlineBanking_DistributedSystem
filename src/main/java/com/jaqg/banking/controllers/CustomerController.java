065c1e439275c50363f90e5f5d43d8da43374479
[Quiara] <[quiara.butler@peopleshores.com]>
2024-08-26 12:01:46 -0400

    updates

--- src/main/java/com/jaqg/banking/controllers/CustomerController.java
+++ src/main/java/com/jaqg/banking/controllers/CustomerController.java
@@ -1,20 +1,46 @@
 package com.jaqg.banking.controllers;
 import com.jaqg.banking.entities.Account;
 import com.jaqg.banking.entities.Customer;
+import com.jaqg.banking.repos.CustomerRepo;
 import com.jaqg.banking.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
 
+@RestController
 public class CustomerController {
-    @Autowired
     private CustomerService customerService;
 
-    @GetMapping
-    private List<Account> getAccounts() {
-        return customerService.retrieveAllAccounts;
+
+    public CustomerController(final CustomerRepo customerService) {
+        this.customerService = (CustomerService) customerService;
     }
 
+    @GetMapping("/customer")
+    public List<Customer> retrieveAllCustomers(){
+        return this.customerService.findAll();
     }
+
+    @GetMapping("/customer/{id}")
+    public Customer findCustomerById(@PathVariable Long ID){
+        return this.customerService.getCustomer(ID);
+    }
+
+    @PostMapping("/customer")
+    public Customer addNewCustomer(@RequestBody Customer customer){
+        Customer newCustomer = this.customerService.addNewCustomer(customer);
+        return newCustomer;
+    }
+
+    @DeleteMapping("/customer/{id}")
+    public Customer removeCustomer(
+            @PathVariable("id") Long ID) {
+        Customer customer = this.customerService.updateCustomer(ID);
+        return customer;
+    }
+
+}
 
--- src/main/java/com/jaqg/banking/services/CustomerService.java
+++ src/main/java/com/jaqg/banking/services/CustomerService.java
@@ -1,17 +1,46 @@
 package com.jaqg.banking.services;
-
+import com.jaqg.banking.entities.Customer
 import com.jaqg.banking.entities.Account;
+import com.jaqg.banking.repos.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Service;
 import  org.springframework.context.ApplicationContext;
+import org.yaml.snakeyaml.events.Event;
 
 import java.util.List;
+import java.util.Optional;
 
 public class CustomerService {
-    public List<Account> retrieveAllAccounts;
-    @SpringBootApplication
+    //Repo injection
+    @Autowired
+    private CustomerRepo customerRepo;
 
-    ApplicationContext context = SpringBootApplication.run(CustomerService.class, args);
-    CustomerService customerService = context.getBean(CustomerService.class);
+    public Customer getCustomer(Long ID){
+        return customerRepo.findById(ID)
+    }
+
+    public List<Customer> findAll() {
+        return customerRepo.findAll();
+    }
+
+    public Customer addNewCustomer(Customer customer){
+        return customerRepo.save(customer);
+    }
+
+    public Customer updateCustomer(Long ID) {
+        Optional<Customer> optionalCustomer = customerRepo.findById(ID);
+        if (optionalCustomer.isPresent()) {
+            Customer customer = optionalCustomer.get();
+            customer.setRemoved(true);
+            customerRepo.save(customer)
+        } else {
+            return null;
+        }
+
+
+    }
+
+
 
 }
